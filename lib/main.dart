c5a068f804403554ecc2d2f7039ca6db29d6902e
Shirish Kadam <mail@5hirish.com>
2023-04-16 19:54:35 +0530

    Restricted minimum windown size

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:window_manager/window_manager.dart';
 
 // ignore: depend_on_referenced_packages
 import 'package:flutter_web_plugins/url_strategy.dart';
@@ -7,10 +8,16 @@ import 'package:flutter_web_plugins/url_strategy.dart';
 import 'core.dart';
 import 'utils/state_logger.dart';
 
-void main() {
+void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
 
-  // turn off the # in the URLs on the web
+  await windowManager.ensureInitialized();
+
+  // Set the minimum window size
+  windowManager.setMinimumSize(const Size(500, 500));
+
+  // Turn off the # in the URLs on the web
   usePathUrlStrategy();
 
   runApp(
